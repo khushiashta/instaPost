83f59399c9cb8fde8f2527fe8a5501af4884519e
Khushi Ashta <khushiashta@192.168.1.3>
2023-07-30 16:59:00 +0530

    added more functions

--- post.py
+++ post.py
@@ -1,4 +1,21 @@
 from instabot import Bot
 bot = Bot()
+
+#login
 bot.login(username = ("username"),password = ("password"))
+
+#upload photo
 bot.upload_photo("yoda.jpeg", caption="biscuit eating baby")
+
+#following
+bot.follow("elonrmuskk")
+
+#send a message
+bot.send_message("Hello", ['user1','user2'])
+
+#get follower info
+my_followers = bot.get_user_followers("dhavalsays")
+for follower in my_followers:
+    print(follower)
+
+bot.unfollow_everyone()
